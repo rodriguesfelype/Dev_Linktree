e21736f98ac33d6b960a4c0d43845673c7af7fe5
Felype Deschamps <rodriguesfelype2@gmail.com>
2023-09-08 20:59:50 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode(){
+  const html = document.documentElement
+  html.classList.toggle('light')
+}
+
+//function toggleMode(){
+  //const html = document.documentElement
+  //if (html.classList.contains('light')){
+  //  html.classList.remove('light')
+  //}else{
+  //  html.classList.add('light')
+  //}
+//}
